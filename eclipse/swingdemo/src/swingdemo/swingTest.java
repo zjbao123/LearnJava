91afd036e8424d97dc35a167f21eda027cb549e9
zjbao123 <345232213@qq.com>
2016-11-24 22:05:29 +0800

    11.23

--- eclipse/swingdemo/src/swingdemo/swingTest.java
+++ eclipse/swingdemo/src/swingdemo/swingTest.java
@@ -5,8 +5,10 @@ import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.TextArea;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,9 +16,12 @@ import java.util.List;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
 import javax.swing.BorderFactory;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
@@ -26,6 +31,8 @@ import javax.swing.JTable;
 import java.awt.Color;
 import java.awt.event.ItemListener;
 import java.awt.event.ItemEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class swingTest extends JFrame {
 
@@ -57,7 +64,6 @@ public class swingTest extends JFrame {
 	public swingTest() {
 		JTextArea textArea = new JTextArea();
 
-		
 		String valueString;
 		String[] elements;
 		List<Student> students = new ArrayList<Student>();
@@ -74,7 +80,7 @@ public class swingTest extends JFrame {
 			System.out.println(e);
 
 		}
-		
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1000, 600);
 		contentPane = new JPanel();
@@ -95,14 +101,71 @@ public class swingTest extends JFrame {
 				textArea.setText("");
 				textArea.append(students.get(comboBoxSname.getSelectedIndex())
 						.toString());
-				
+
 			}
 		});
-		
+
 		comboBoxSname.setBorder(BorderFactory.createTitledBorder("选择一个学生："));
 		contentPane.add(comboBoxSname, BorderLayout.NORTH);
 
 		JButton btnNewButton = new JButton("保存至文件");
+		btnNewButton.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// TODO Auto-generated method stub
+				int option = JOptionPane.showConfirmDialog(null, "文件已修改，是否保存？",
+						"保存文件？", JOptionPane.YES_NO_OPTION,
+						JOptionPane.WARNING_MESSAGE, null);
+				switch (option) {
+				case JOptionPane.YES_NO_OPTION: {
+					FileWriter fw;
+					try {
+						fw = new FileWriter("Java课成绩单.txt");
+
+						BufferedWriter bw = new BufferedWriter(fw);
+						for (Student student : students) {
+							bw.write((students.indexOf(student)+1) + "\t" +student.sCollege + "\t" + student.sClass
+									+ "\t" + student.sNo + "\t" + student.sName
+									+ " \t" + student.sjava + "\r\n");
+						}
+						bw.close();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					JOptionPane.showMessageDialog(null, "保存完成！", "系统信息",
+							JOptionPane.DEFAULT_OPTION);
+					break;
+				}
+				}
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+		});
 		contentPane.add(btnNewButton, BorderLayout.SOUTH);
 
 		textArea.setBorder(BorderFactory.createTitledBorder("该学生信息为："));
@@ -110,21 +173,48 @@ public class swingTest extends JFrame {
 		textArea.append(students.get(comboBoxSname.getSelectedIndex())
 				.toString());
 		contentPane.add(textArea, BorderLayout.CENTER);
-		
-		
-		final Object[] columnNames = {"专业", "班级", "学号","姓名", "java"};
+
+		final Object[] columnNames = { "专业", "班级", "学号", "姓名", "java" };
 		final Object[][] rowData = new Object[60][5];
 		for (Student student : students) {
-			rowData[students.indexOf(student)]=student.array();
+			rowData[students.indexOf(student)] = student.array();
 		}
-		System.out.println(rowData);
-		JTable table = new JTable(rowData,columnNames);
-		
-		JScrollPane pane1 = new JScrollPane (table);
-		table.setPreferredScrollableViewportSize(new Dimension(600, 100)); 
-		contentPane.add(pane1,BorderLayout.EAST);
-	}
+		JTable table = new JTable(rowData, columnNames);
+		table.setPreferredScrollableViewportSize(new Dimension(600, 100));
+		table.getModel().addTableModelListener(new TableModelListener() {
 
+			@Override
+			public void tableChanged(TableModelEvent e) {
+				// TODO Auto-generated method stub
+				if (e.getType() == TableModelEvent.UPDATE) {
+					String newvalue = table.getValueAt(e.getLastRow(),
+							e.getColumn()).toString();
+					if (!newvalue.equals("")){
+					if (e.getColumn() == 0) {
+						students.get(e.getLastRow()).setsCollege(newvalue);
+					}
+					if (e.getColumn() == 1) {
+						students.get(e.getLastRow()).setsClass(newvalue);
+					}
+					if (e.getColumn() == 2) {
+						students.get(e.getLastRow()).setsNo(newvalue);
+					}
+					if (e.getColumn() == 3) {
+						students.get(e.getLastRow()).setsName(newvalue);
+					}
+					if (e.getColumn() == 4) {
+						students.get(e.getLastRow()).setSjava(newvalue);
+					}
+					}
+					textArea.setText("");
+					textArea.append(students.get(
+							comboBoxSname.getSelectedIndex()).toString());
+				}
+			}
+		});
+		JScrollPane pane1 = new JScrollPane(table);
+		contentPane.add(pane1, BorderLayout.EAST);
+	}
 }
 
 class Student {
@@ -132,12 +222,14 @@ class Student {
 	String sName;
 	String sNo;
 	String sCollege;
+	String sjava;
 
 	public Student(String sCollege, String sClass, String sNo, String sName) {
 		this.sClass = sClass;
 		this.sCollege = sCollege;
 		this.sName = sName;
 		this.sNo = sNo;
+		sjava = null;
 	}
 
 	public String getsName() {
@@ -147,25 +239,53 @@ class Student {
 	public String getsClass() {
 		return sClass;
 	}
+
 	public String getsCollege() {
 		return sCollege;
 	}
+
 	public String getsNo() {
 		return sNo;
 	}
+
+	public String getSjava() {
+		return sjava;
+	}
+
+	public void setSjava(String sjava) {
+		this.sjava = sjava;
+	}
+
+	public void setsClass(String sClass) {
+		this.sClass = sClass;
+	}
+
+	public void setsCollege(String sCollege) {
+		this.sCollege = sCollege;
+	}
+
+	public void setsName(String sName) {
+		this.sName = sName;
+	}
+
+	public void setsNo(String sNo) {
+		this.sNo = sNo;
+	}
+
 	@Override
 	public String toString() {
 		return ("专业为\t" + sCollege + "\n" + "班级为\t" + sClass + "\n" + "学号为\t"
-				+ sNo + "\n" + "姓名为\t" + sName + "\n");
+				+ sNo + "\n" + "姓名为\t" + sName + "\n" + "java成绩为\t" + sjava + "\n");
 	}
-	public Object[] array(){
+
+	public Object[] array() {
 		Object[] a = new Object[5];
 		a[0] = getsCollege();
 		a[1] = getsClass();
 		a[2] = getsNo();
 		a[3] = getsName();
-		a[4] = null;
+		a[4] = getSjava();
 		return a;
-		
+
 	}
 }
